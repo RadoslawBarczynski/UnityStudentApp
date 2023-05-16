94c9588b89985842f636ecab5c7ce8b1d88578f1
Radosław <teammenpl@gmail.com>
2023-05-16 22:35:28 +0200

    Update database context

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Supabase.Client;
+using static UnityEditor.Progress;
 
 public class GameManager : MonoBehaviour
 {
+    //db
+    string url;
+    string key;
+    public Supabase.Client client;
+
     //gameobjects
     public GameObject HomeworkParent;
     
@@ -33,12 +42,6 @@ public class GameManager : MonoBehaviour
             }
             Debug.Log(homework.id);                  
         }
-
-        foreach(var item in userDataLogged.homeworksToDelete)
-        {
-            Debug.Log(item.id);
-        }
-
         
     }
 
@@ -63,6 +66,39 @@ public class GameManager : MonoBehaviour
                 return;
             }
         }
+    }
+
+    public void TestScoreAdd()
+    {
+        Guid id = new Guid("7217e7cc-c37c-48f7-ba5e-0f9892a69c90");
+
+        UpdateScore(5, id);
+    }
+
+    public async void UpdateScore(int scoreToAdd, Guid gradeId)
+    {
+        url = "https://melfibfnkmadpskpvist.supabase.co";
+        key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lbGZpYmZua21hZHBza3B2aXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxOTIxMTcsImV4cCI6MTk5Mzc2ODExN30.Kd7kp1eiKHzcTsg7noH02E_smiAJR_Y-9kR45wg6UlE";
+
+        client = await Supabase.Client.InitializeAsync(url, key, new Supabase.SupabaseOptions
+        {
+            AutoConnectRealtime = true
+        });
+
+        //grades table
+        var grades = await client.From<Grade>().Get();
+        List<Grade> tempGrades = grades.Models;
+
+        foreach (var grado in tempGrades)
+        {
+            if(grado.GradeId == gradeId)
+            {
+                grado.Score += scoreToAdd;
+                await grado.Update<Grade>();
+            }
+        }
+      
+
     }
 
 
